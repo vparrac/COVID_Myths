d27f448f9ab760b9885bc1b1760dd8deec213f32
Valerie Parra Cortés <v.parrac@uniandes.edu.co>
2020-05-08 12:34:52 -0500

    Add layout

---
+++ covidMyths/front/src/layout/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import PropTypes from 'prop-types';
+import {Link} from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <div className="container-fluid">
+      <div className="footer">
+        <hr></hr>
+        <h4>Covid-19 Symtoms <br></br>
+        Icons made by {" "}
+        <href to="https://www.flaticon.com/authors/freepik" title="Freepik">
+          Freepik
+        </href>{" "}
+        from{" "}
+        <href to="https://www.flaticon.com/" title="Flaticon">
+          {" "}
+          www.flaticon.com
+        </href></h4>
+      </div>
+    </div>
+  );
+};
+
+// Footer.propTypes = {
+
+// };
+
+export default Footer;
